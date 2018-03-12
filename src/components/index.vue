fdd9d0b2bfb3ab9a27f1853d53abec12a0fb81ee
Kevin <kevin@Yans-MacBook-Air.local>
2018-03-12 17:59:17 -0600

    index page

---
+++ src/components/index.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="welcome">
+        <div>
+            <h1>{{ welcome }}</h1>
+        </div>
+
+        <div>
+            <h2>{{ description }} </h2>
+        </div>
+
+        <div id="app-5">
+            <p>{{ message }}</p>
+            <p><button>sign up</button> <button>log in</button></p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'signup',
+  data () {
+    return {
+      welcome: 'Welcome',
+      description: 'This is a government website allow user download their desired software',
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+button {
+    background-color: #0098DB; /* Green */
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import signup from '@/components/signup'
+import index from '@/components/index'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: '',
+      component: index
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: signup
     }
   ]
 })
