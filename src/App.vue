75d2261833516cf429cd0ec5d3ebed4bd1f77b95
wellsjonathon <wellsjonathonr@gmail.com>
2018-03-12 16:49:40 -0600

    Test push

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <div id="nav">
+      <div id="nav__top">
+
+      </div>
+      <div id="nav__bottom">
+
+      </div>
+    </div>
     <router-view/>
   </div>
 </template>
@@ -12,12 +19,32 @@ export default {
 </script>
 
 <style>
+html, body {
+  padding: 0;
+  margin: 0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  width: 100vw;
+  height: 100vh;
+
+  display: grid;
+  grid-template-rows: 1fr 5fr 1fr;
+  background: #F4F4F5;
+}
+#nav {
+  grid-row: 1 / 2;
+  background: #2c3e50;
+}
+#nav__top {
+  background: #2c3e50;
+}
+#nav__bottom {
+  background: #0098DB;
 }
 </style>
