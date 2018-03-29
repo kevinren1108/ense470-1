1cb2bf4273b09bfbc31bab086373e3e0a1986952
wellsjonathon <wellsjonathonr@gmail.com>
2018-03-28 18:26:46 -0600

    Fixed DB variable

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -3,12 +3,12 @@ const Op = Sequelize.Op
 module.exports = {
   port: process.env.PORT || 8080,
   db: {
-    database: process.env.DB_NAME || 'mylocaldb',
+    database: process.env.DB_NAME || 'ense470',
     user: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASS || '1234',
+    password: process.env.DB_PASS || 'ense470',
     options: {
       dialect: process.env.DIALECT || 'postgres',
-      host: process.env.HOST || 'localhost',
+      host: process.env.DB_HOST || 'localhost',
       // storage: './ense470.sqlite',
       operatorsAliases: Op
     }
