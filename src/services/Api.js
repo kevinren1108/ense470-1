dab23e9b847b05c2c09edd539c6644be9932ee17
wellsjonathon <wellsjonathonr@gmail.com>
2018-03-28 18:33:22 -0600

    Fix baseURL

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default () => {
   return axios.create({
-    baseURL: `http://localhost:8080/`
+    baseURL: (process.env.DB_HOST || 'localhost') + ':' + (process.env.PORT || 8080)
   })
 }
 
