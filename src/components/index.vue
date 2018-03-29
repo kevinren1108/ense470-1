8e8872e24edd4ea2e83dca479f2c29282de19cf6
wellsjonathon <wellsjonathonr@gmail.com>
2018-03-28 22:04:04 -0600

    Conditional rendering on home page and nav bar

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,24 +1,47 @@
 <template>
     <div class="page">
         <div class="page__content">
-          <div class="page__title-container">
-            <h1 class="page__title">Welcome</h1>
-          </div>
-          <div>
-            <p>
-              This application is a portal for requesting software.
-              If you are an approver, you can manage the requests for
-              your software here as well.
-            </p>
-            <br>
-            <h3>
-              Please login to continue
-            </h3>
-          </div>
-          <div class="page__btn-container">
-            <button class="btn--blue btn--large btn--main" @click="$router.push('/login')">Log in</button>
-            <button class="btn--blue btn--large btn--main" @click="$router.push('/signup')">Sign up</button>
-          </div>
+          <template v-if="!this.$store.state.isUserLoggedIn">
+            <div class="page__title-container">
+              <h1 class="page__title">Welcome</h1>
+            </div>
+            <div>
+              <p>
+                This application is a portal for requesting software.
+                If you are an approver, you can manage the requests for
+                your software here as well.
+              </p>
+              <br>
+              <h3>
+                Please login to continue
+              </h3>
+            </div>
+            <div class="page__btn-container">
+              <button class="btn--blue btn--large btn--main" @click="$router.push('/login')">Log in</button>
+              <button class="btn--blue btn--large btn--main" @click="$router.push('/signup')">Sign up</button>
+            </div>
+          </template>
+          <template v-else>
+            <div class="page__title-container">
+              <h1 class="page__title">Welcome, {{this.$store.state.user.first__name}}</h1>
+            </div>
+            <div class='page__content'>
+              <div class="page__title-container page__title-container--button">
+                <h1 class="page__title">My Software</h1>
+              </div>
+              <div class="list-container">
+                <ul class="list">
+                  <div v-for="item in software" :key="item.id">
+                    <li class="list__item list__item--request">
+                      <span class="list__item__title">{{item.name}}</span>
+                      <span class="list__item__btn-container"><button class="btn--blue list__item__btn" @click="$router.push('/request/' + request.id)">View</button></span>
+                    </li>
+                    <hr>
+                  </div>
+                </ul>
+              </div>
+            </div>
+          </template>
           <!-- <div id="row-1"><input v-on:change="checkEmail()" type="text" class="login__input" id="email" placeholder="Email"/><div id="em__msg"></div></div>
           <div id="row-2"><input v-on:change="checkPassword()" type="password" class="login__input" id="password" placeholder="Password"/><div id="pw__msg"></div></div>
           <div id="row-3">
@@ -34,57 +57,20 @@ export default {
   name: 'index',
   data () {
     return {
-      description: 'This is a portal for requesting access to any software you may need. Please log in to continue.'
+      description: 'This is a portal for requesting access to any software you may need. Please log in to continue.',
+      software: [
+        {
+          name: 'Generic Software 1',
+          id: 0
+        },
+        {
+          name: 'Unique Software',
+          id: 1
+        }
+      ]
     }
   },
   methods: {
-    validate: function () {
-      var email = document.getElementById('email').value
-      var emErrorMsg = ''
-      if (!(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email))) {
-        emErrorMsg += 'You have entered an invalid email address <br> '
-      }
-      if (emErrorMsg.length > 0) {
-        document.getElementById('em__msg').innerHTML = emErrorMsg
-      } else {
-        document.getElementById('em__msg').innerHTML = ''
-      }
-      // password
-      var password = document.getElementById('password').value
-      var pwErrorMsg = ''
-      if (password.length < 8) {
-        pwErrorMsg += 'You have to enter at least 8 characters <br> '
-      }
-      if (pwErrorMsg.length > 0) {
-        document.getElementById('pw__msg').innerHTML = pwErrorMsg
-      } else {
-        document.getElementById('pw__msg').innerHTML = ''
-      }
-    },
-    checkEmail: function () {
-      var email = document.getElementById('email').value
-      var errorMsg = ''
-      if (!(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email))) {
-        errorMsg += 'You have entered an invalid email address'
-      }
-      if (errorMsg.length > 0) {
-        document.getElementById('em__msg').innerHTML = errorMsg
-      } else {
-        document.getElementById('em__msg').innerHTML = ''
-      }
-    },
-    checkPassword: function () {
-      var password = document.getElementById('password').value
-      var errorMsg = ''
-      if (password.length < 8) {
-        errorMsg += 'You have to enter at least 8 characters'
-      }
-      if (errorMsg.length > 0) {
-        document.getElementById('pw__msg').innerHTML = errorMsg
-      } else {
-        document.getElementById('pw__msg').innerHTML = ''
-      }
-    }
   }
 }
 </script>
@@ -128,6 +114,64 @@ export default {
 }
 #login__submit {
   padding: 16px 64px;
+}
+.list-container {
+  width: 100%;
+  border: 1px solid rgba(74, 84, 90, 0.4);
+  border-radius: 4px;
+  background: #FBFBFB;
+  padding: 6px 6px;
+  box-sizing: border-box;
+}
+.list {
+  margin: 0;
+}
+.list__btn-container {
+  padding: 0 10px;
+}
+.list__btn--full {
+  width: 100%;
+  border-radius: 4px;
+  padding: 17px 32px;
+  margin: 5px 0;
+  box-shadow: 1px 1px 4px 0 rgba(0,0,0,0.1),
+              2px 4px 4px 0 rgba(0,0,0,0.05);
+}
+.list__item {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0;
+  padding: 5px 10px;
+  border-radius: 4px;
+}
+.list__item:hover {
+  transition: all 0.2s ease 0s;
+  background: #EEEEEE;
+}
+.list__item__title {
+  width: 40%;
+  text-align: left;
+}
+.list__item__status {
+  width: 40%;
+  text-align: center;
+}
+.list__item__btn-container {
+  width: 20%;
+  text-align: right;
+}
+.list__item__btn {
+  border-radius: 4px;
+  padding: 12px 32px;
+  box-shadow: 1px 1px 4px 0 rgba(0,0,0,0.1),
+              2px 4px 4px 0 rgba(0,0,0,0.05);
+}
+hr {
+  margin: 6px;
+  border: 0;
+  height: 1px;
+  background: rgba(31, 44, 51, 0.1);
 }
 h1, h2, p {
   text-align: left;
